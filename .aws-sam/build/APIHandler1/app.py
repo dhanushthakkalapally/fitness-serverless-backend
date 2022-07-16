27f674929e8d39603a02cf2ea3395acbf8772e90
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-07-16 11:59:02 +0100

    added all the files

---
+++ .aws-sam/build/APIHandler1/app.py
@@ -0,0 +1,22 @@
+import json
+
+def lambda_handler(event, context):
+    username = event["requestContext"]["authorizer"]["claims"]["preferred_username"]
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"message": "welcome %s"%username}),
+        "headers": {
+            "Content-type": "application/json"
+        }
+    }
+
+
+def lambda_handler1(event, context):    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"message": "welcome from the new lambda handler"}),
+        "headers": {
+            "Content-type": "application/json"
+        }
+    }
