d8b3390fa727aef133572dd65c08adff455b37bd
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-07-16 19:44:47 +0100

    now we can store calorie info in the database

--- src/create_daily_calories/app.py
+++ src/create_daily_calories/app.py
@@ -4,8 +4,8 @@ import datetime
 import os
 
 # {
-#     "date": "2022-07-16",
-#     "nutritional_info": {
+    # now we only support four categories, but in future we may support more :) we don't know yet
+#     "consumptions": {
 #         "breakfast": {"items": [{"name": "string", "calories": "number"}]}, 
 #         "lunch": {"items": [{"name": "string", "calories": "number"}]}, 
 #         "dinner": {"items": [{"name": "string", "calories": "number"}]}, 
@@ -14,21 +14,44 @@ import os
 # }
 
 region_name = os.environ["REGION_NAME"]
-client = boto3.client("dynamodb", region_name=region_name)
+dynamodb = boto3.resource("dynamodb", region_name=region_name)
 TABLE_NAME = os.environ.get("TABLE_NAME") 
+table = dynamodb.Table(TABLE_NAME)
 
 def lambda_handler(message, context):
     user_uuid = message["pathParameters"]["user_uuid"]
     date = message["pathParameters"]["date"]
-    # user_uuid = message["requestContext"]["authorizer"]["claims"]["sub"]
-    # request_body = json.loads(message["body"])
-    # date = request_body.get("date", str(datetime.datetime.now().date()))
-    # calorie_dict = {"morning": {"SS": ["eggs"]}, "lunch": {"SS": ["milk"]}, "dinner": {"SS": ["water"]}, "user_uuid": {"S": user_uuid}, "date": {"S": date}}
-    
-    # response = client.put_item(TableName = TABLE_NAME, Item=calorie_dict)
+    if user_uuid and date:
+        #TODO: make sure user doesn't send the date from the future and also actually sends the date
+        #TODO: Also check if the users is valid in the cognito before creating the record
+        #TODO: Validate the schema of the data that we are receiving
+        request_body = json.loads(message["body"])
+        daily_calorie_consumptions = {"user_uuid": user_uuid,
+                                      "date": date, 
+                                      "consumptions": {}, 
+                                      "total_calories": 0}
+        
+        consumptions = request_body.get("consumptions", {})
+        for consumption_type in consumptions.keys():
+            if consumption_type in ["breakfast", "dinner", "lunch", "snacks"]:
+                consumption = consumptions[consumption_type]
+                calories = 0
+                items = consumption["items"]
+                for item in items:
+                    calories += item["calories"]
+                consumption["meal_calories"] = calories
+                daily_calorie_consumptions["consumptions"][consumption_type] = consumption 
+                daily_calorie_consumptions["total_calories"] += calories 
 
+        table.put_item(Item=daily_calorie_consumptions)
+
+    else:
+        return {
+            "statusCode": 400,
+            "headers": {"application/json"},
+            "body": json.dumps({"body": "invalid Request"})
+        }
     return {
         "statusCode": 201,
-        "headers": {},
-        "body": json.dumps({"user_uuid": user_uuid, "date": date})
+        "headers": {}
     }
