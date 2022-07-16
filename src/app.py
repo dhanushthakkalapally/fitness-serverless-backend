d950a6e85105546871ebf4eb80184d54b4f5298b
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-07-16 11:29:51 +0100

    added another lambda handler

--- src/app.py
+++ src/app.py
@@ -10,3 +10,13 @@ def lambda_handler(event, context):
             "Content-type": "application/json"
         }
     }
+
+
+def lambda_handler1(event, context):    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"message": "welcome from the new lambda handler"}),
+        "headers": {
+            "Content-type": "application/json"
+        }
+    }
