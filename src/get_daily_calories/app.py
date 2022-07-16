308485240f75b724bb2f6b37762bbcad5554b00b
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-07-16 20:05:57 +0100

    created api to get items

--- src/get_daily_calories/app.py
+++ src/get_daily_calories/app.py
@@ -1,18 +1,25 @@
 import json
+import boto3
+import os
 
+region_name = os.environ["REGION_NAME"]
+dynamodb = boto3.resource("dynamodb", region_name=region_name)
+TABLE_NAME = os.environ.get("TABLE_NAME") 
+table = dynamodb.Table(TABLE_NAME)
 
 def lambda_handler(message, context):
     user_uuid = message["pathParameters"]["user_uuid"]
     date = message["pathParameters"]["date"]
-    # user_uuid = message["requestContext"]["authorizer"]["claims"]["sub"]
-    # request_body = json.loads(message["body"])
-    # date = request_body.get("date", str(datetime.datetime.now().date()))
-    # calorie_dict = {"morning": {"SS": ["eggs"]}, "lunch": {"SS": ["milk"]}, "dinner": {"SS": ["water"]}, "user_uuid": {"S": user_uuid}, "date": {"S": date}}
-    
-    # response = client.put_item(TableName = TABLE_NAME, Item=calorie_dict)
+
+    response = table.get_item(
+        Key={
+            'user_uuid': user_uuid,
+            'date': date
+        }
+    )
 
     return {
-        "statusCode": 201,
+        "statusCode": 200,
         "headers": {},
-        "body": json.dumps({"user_uuid": user_uuid, "date": date})
+        "body": json.dumps(response["Items"])
     }
